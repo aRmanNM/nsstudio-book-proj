29eca483bf875a7ac88018ef10324f5680cde0cd
armannm <anm4500@gmail.com>
2022-01-21 00:57:50 +0330

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,8 @@
-import { NgModule } from '@angular/core'
-import { Routes } from '@angular/router'
-import { NativeScriptRouterModule } from '@nativescript/angular'
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { NativeScriptRouterModule } from "@nativescript/angular";
 
-import { ItemsComponent } from './item/items.component'
-import { ItemDetailComponent } from './item/item-detail.component'
-
-const routes: Routes = [
-  { path: '', redirectTo: '/items', pathMatch: 'full' },
-  { path: 'items', component: ItemsComponent },
-  { path: 'item/:id', component: ItemDetailComponent },
-]
+const routes: Routes = [{ path: "", redirectTo: "/items", pathMatch: "full" }];
 
 @NgModule({
   imports: [NativeScriptRouterModule.forRoot(routes)],

b3f7da9d69f39330b1ca4904ea2a5b6870e1e794
armannm <anm4500@gmail.com>
2022-01-22 20:39:37 +0330

    feature modules added

---
+++ src/app/modules/player/player.module.ts
@@ -0,0 +1,11 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "@nativescript/angular";
+
+@NgModule({
+  imports: [NativeScriptModule],
+  exports: [],
+  declarations: [],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class PlayerModule {}

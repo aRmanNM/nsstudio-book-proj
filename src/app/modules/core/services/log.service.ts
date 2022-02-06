d3ab9cea3f7cee08dce7b37e13382e75474b45b5
armannm <anm4500@gmail.com>
2022-02-06 18:16:19 +0330

    log service implemented

--- src/app/modules/core/services/log.service.ts
+++ src/app/modules/core/services/log.service.ts
@@ -2,5 +2,30 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class LogService {
-  constructor() {}
+  static ENABLE: boolean = true;
+
+  debug(msg: any, ...formatParams: any[]) {
+    if (LogService.ENABLE) {
+      console.log(msg, formatParams);
+    }
+  }
+
+  error(msg: any, ...formatParams: any[]) {
+    if (LogService.ENABLE) {
+      console.error(msg, formatParams);
+    }
+  }
+
+  inspect(obj: any) {
+    if (LogService.ENABLE) {
+      console.log(obj);
+      console.log("typeof: ", typeof obj);
+      if (obj) {
+        console.log("constructor: ", obj.constructor.name);
+        for (let key in obj) {
+          console.log(`${key}: `, obj[key]);
+        }
+      }
+    }
+  }
 }

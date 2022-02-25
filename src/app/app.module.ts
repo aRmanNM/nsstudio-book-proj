5f246f3ab7ccd3230e447406735e786aabdac421
armannm <anm4500@gmail.com>
2022-02-25 23:59:33 +0330

    added core and player module to bootstrapping module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
 
+import { CoreModule } from "./modules/core/core.module";
+import { PlayerModule } from "./modules/player/player.module";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
+  imports: [NativeScriptModule, AppRoutingModule, CoreModule, PlayerModule],
   declarations: [AppComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
